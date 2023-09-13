0068e6fbdbd76ee25f247cbefd9988371c382d9b
George <geoster3d@gmail.com>
2023-09-14 00:18:35 +0700

    drivers: regulator: Import Nokia changes

--- drivers/regulator/qpnp-labibb-regulator.c
+++ drivers/regulator/qpnp-labibb-regulator.c
@@ -3610,6 +3610,16 @@ static int register_qpnp_lab_regulator(struct qpnp_labibb *labibb,
 			return rc;
 		}
 	}
+
+	if (of_property_read_bool(of_node,
+			"qcom,qpnp-lab-enable-pfm-mode")) {
+		pr_info("Lab PFM enable");
+		rc = qpnp_lab_pfm_enable(labibb);
+		if (rc < 0){
+			pr_err("Failed to register lab pfm rc=%d\n",rc);
+		}
+	}
+
 	rc = qpnp_labibb_read(labibb, labibb->lab_base + REG_LAB_MODULE_RDY,
 				&val, 1);
 	if (rc < 0) {
@@ -3670,6 +3680,7 @@ static int register_qpnp_lab_regulator(struct qpnp_labibb *labibb,
 	return 0;
 }
 
+#ifdef CONFIG_FIH_NB1
 static int qpnp_ibb_pfm_mode_enable(struct qpnp_labibb *labibb,
 			struct device_node *of_node)
 {
@@ -3722,6 +3733,7 @@ static int qpnp_ibb_pfm_mode_enable(struct qpnp_labibb *labibb,
 
 	return rc;
 }
+#endif
 
 static int qpnp_labibb_pbs_mode_enable(struct qpnp_labibb *labibb,
 			struct device_node *of_node)
@@ -4805,12 +4817,14 @@ static int register_qpnp_ibb_regulator(struct qpnp_labibb *labibb,
 		}
 	}
 
+#ifdef CONFIG_FIH_NB1
 	if (of_property_read_bool(of_node,
 			"qcom,qpnp-ibb-enable-pfm-mode")) {
 		rc = qpnp_ibb_pfm_mode_enable(labibb, of_node);
 		if (rc < 0)
 			return rc;
 	}
+#endif
 
 	if (labibb->pbs_control) {
 		rc = qpnp_labibb_pbs_mode_enable(labibb, of_node);
